6385ac7d49894318085f7548d59345177ee8ff52
GamTam <luccadacambra@gmail.com>
2020-11-14 14:37:39 -0500

    GradeConverter finished

--- src/GradeConverter/GradeConverter.java
+++ src/GradeConverter/GradeConverter.java
@@ -7,39 +7,161 @@ Description: The program manages the grades for multiple people
 
 package GradeConverter;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import javax.sound.sampled.*;
+import java.io.*;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
-import java.util.Hashtable;
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.*;
 
 public class GradeConverter {
     static Scanner scan = new Scanner(System.in);
+    static Hashtable<Integer, List<String>> grades = new Hashtable<>();
+    static Sound textSound = new Sound();
 
     public static void main(String[] args) throws InterruptedException, IOException {
-        textBox("Welcome to Grade Tracker!", true);
+        textSound.sound = "textbox blip";
+        textBox("\nWelcome to Grade Tracker!", true);
         textBox("This application is designed to help you with knowing what your grades are!\n", true);
         wait(0.5);
-        textBox("Please select someone for grade management:\n", true);
+        boolean done = false;
+        while (!done) {
+            textBox("Please select someone for grade management:\n", true);
 
-        Path path = FileSystems.getDefault().getPath("").toAbsolutePath();
-        File file = new File(path + "/Names");
-        if (!file.exists()) {
-            file.mkdirs();
+            Path path = FileSystems.getDefault().getPath("").toAbsolutePath();
+            File file = new File(path + "/Names");
+
+            if (!file.exists()) {
+                file.mkdirs();
+            }
+            File person;
+            person = fileSelect(file, "person", "");
+
+            textBox("Now, choose which subject you wish to grade them in.\n", true);
+            File gradeCategory = fileSelect(person, "subject", ".csv");
+
+            textBox("Now, choose to add a new assessment or modify previous assessments.\n", true);
+            grades = getCSV(gradeCategory);
+            int grade = selectInt(1, grades.size() + 1);
+
+            if (grade <= grades.size()) {
+                textBox("What is the new grade for " + grades.get(grade).get(0) + "? ", true);
+                int newMark = selectInt(0, 100);
+                List list = new ArrayList();
+                list.add(grades.get(grade).get(0));
+                list.add(Integer.toString(newMark));
+                grades.put(grade, list);
+            } else {
+                textBox("Enter the title of the assessment: ", false);
+                String name = selectWord();
+                textBox("And what is the grade for " + name + "? ", true);
+
+                int newMark = selectInt(0, 200);
+                List list = new ArrayList();
+                list.add(name);
+                list.add(Integer.toString(newMark));
+                grades.put(grade, list);
+            }
+
+            updateGrades(gradeCategory, grades);
+
+            textBox("\nThank you for using the Grade Converter! The file is available at:\n ", true);
+            System.out.println(gradeCategory);
+            wait(1.0);
+
+            textBox("\nDo you want to see the full chart?", true);
+            textBox("[1] Yes", true);
+            textBox("[2] No", true);
+
+            int cont = selectInt(1, 2);
+
+            boolean show = false;
+            if (cont == 1) {
+                show = true;
+            }
+
+            if (show) {
+                textBox(String.format("\n%2$-50s %3$-20s %4$-15s", " ", "Assesment Title", "Number Grade", "Letter Grade"), 25, true);
+                textBox(String.format("%2$-50s %3$-20s %4$-15s", " ", "---------------", "------------", "------------"), 25, true);
+                for (int i = 1; i < grades.size() + 1; i++) {
+                    textBox(String.format("%2$-50s %3$-20s %4$-15s", " ", grades.get(i).get(0), grades.get(i).get(1), toLetter(grades.get(i).get(1))), 25, true);
+                }
+            }
+
+            textBox("\nDo you wish to modify/add more people?", true);
+            textBox("[1] Yes", true);
+            textBox("[2] No", true);
+
+            cont = selectInt(1, 2);
+
+            if (cont == 2) {
+                done = true;
+            }
         }
-        File person;
-        person = fileSelect(file, "person", "");
 
-        textBox("Now, choose which subject you wish to grade them in.\n", true);
-        File gradeCategory = fileSelect(person, "subject", ".csv");
+        textBox("Goodbye!", false);
+    }
+
+    public static void updateGrades(File file, Hashtable<Integer, List<String>> grades) throws IOException {
+        FileWriter writer = new FileWriter(file);
+        writer.write("Assessment,Number Grade,Letter Grade\n");
+        for (int i = 1; i < grades.size() + 1; i++) {
+            String letter = toLetter(grades.get(i).get(1));
+            String str = grades.get(i).get(0) + "," + grades.get(i).get(1) + "," + letter + "\n";
+            writer.write(str);
+        }
+        writer.close();
+    }
+
+    public static String toLetter(String input) {
+        int num = Integer.parseInt(input);
+
+        if (num >= 95) {
+            return "A+";
+        } else if (num >= 90) {
+            return "A";
+        } else if (num >= 85) {
+            return "A-";
+        } else if (num >= 80) {
+            return "B";
+        } else if (num >= 70) {
+            return "C";
+        } else if (num >= 60) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+
+    public static Hashtable<Integer, List<String>> getCSV (File file) throws IOException, InterruptedException {
+        Hashtable<Integer, List<String>> table = new Hashtable<>();
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String oneData = "";
+        int i = 0;
+
+        // Loops through CSV and adds data to food class
+        while ((oneData = br.readLine()) != null) {
+            String[] data = oneData.split(",");
+
+            if (i > 0) {
+                List list = new ArrayList();
+                list.add(data[0]);
+                list.add(data[1]);
+                table.put(i, list);
+
+                textBox("[" + i + "]: " + data[0], true);
+            }
+
+            i++;
+        }
+        textBox("[" + i + "]: New Assessment", true);
+
+        return table;
     }
 
     public static File fileSelect(File folder, String type, String fileType) throws InterruptedException, IOException {
         int i = 1;
-        Hashtable<Integer, File> people = new Hashtable<Integer, File>();
+        Hashtable<Integer, File> people = new Hashtable<>();
 
         for (File file : folder.listFiles()) {
             if (file.isDirectory() && fileType.equalsIgnoreCase("")) {
@@ -85,7 +207,7 @@ public class GradeConverter {
                     if (!newFile.exists()) {
                         newFile.createNewFile();
                         FileWriter writer = new FileWriter(newFile);
-                        writer.write("Assesment,Number Grade,Letter Grade");
+                        writer.write("Assessment,Number Grade,Letter Grade");
                         writer.close();
                         return (newFile);
                     } else {
@@ -104,10 +226,12 @@ public class GradeConverter {
     public static void textBox(String text, int time, boolean newLine) throws InterruptedException {
         for (int i = 0; i < text.length() - 1; i++) {
             Thread.sleep(time);
+            textSound.play();
             System.out.print(text.charAt(i));
         }
 
         Thread.sleep(time);
+        textSound.play();
         if (newLine) {
             System.out.println(text.charAt(text.length() - 1));
         } else {
@@ -167,5 +291,43 @@ public class GradeConverter {
         }
 
         return str;
+    }
+}
+
+// A sound
+class Sound {
+    // Variable Init
+    static String sound;
+
+    static Clip clip;
+
+    public static void play() {
+        if (clip != null) {
+            if (clip.isActive()) {
+                clip.stop();
+            }
+        }
+
+        try {
+            // Locate file
+            Path path = FileSystems.getDefault().getPath("").toAbsolutePath();
+            File file = new File(path + "/sounds/" + sound + ".wav");
+
+            // Load and play audio
+            AudioInputStream inputStream = AudioSystem.getAudioInputStream(file);
+            AudioFormat format = inputStream.getFormat();
+            DataLine.Info info = new DataLine.Info(Clip.class, format);
+            clip = (Clip)AudioSystem.getLine(info);
+            clip.open(inputStream);
+            clip.start();
+
+            long i = 0;
+
+            while (!clip.isActive()) {
+                i++;
+            }
+        } catch (Exception e) {
+            System.err.println("Sans Undertale");
+        }
     }
 }
